fb42012fd3a6ce4a619a462c2ebda2fe9474a197
Luciano Martins <luciano.martins@quay.com.br>
2026-07-21 06:50:44 -0300

    feat(admin): add session/thing/device admin registration

--- backend/cdr/admin.py
+++ backend/cdr/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from cdr.models import Organization, Mno, Customer, PricePlan, NetworkProvider, Thing
+from cdr.models import Organization, Mno, Customer, PricePlan, NetworkProvider, Thing, Session, Device
 
 admin.site.register(
     [
@@ -9,18 +9,47 @@ admin.site.register(
         Customer,
         NetworkProvider,
         PricePlan,
-        Thing,
     ]
 )
 
+@admin.register(Session)
+class SessionAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        '__str__',
+        'device',
+        'sessioncreatetime',
+        'realusage',
+        'uom',
+    )
+    list_filter = (
+        'device__imsi',
+        'sessionid',
+    )
+    search_fields = (
+        'sessionid',
 
-# class ThingAdmin(admin.ModelAdmin):
-#     list_display = (
-#         'id',
-#         'thingname',
-#         'thingnameraw',
-#         'thingsgroupname',
-#         'thingsgroupid',
-#         'thingsgroupidraw',
-#     )
-# admin.site.register(Thing, ThingAdmin)
+    )
+
+@admin.register(Thing)
+class ThingAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        '__str__',
+        'thingsgroupid',
+        'customer__customername',
+    )
+
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        '__str__',
+        'iccid',
+        'imsi',
+        'msisdn',
+        'imei',
+    )
+    list_filter = (
+        'imsi',
+    )
