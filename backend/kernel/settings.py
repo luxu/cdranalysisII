0e3fa1d921157cba22313f2a88854983d009d891
Luciano Martins <luciano.martins@quay.com.br>
2026-08-03 11:06:04 -0300

    chore(backend/config): simplify env var names removing backend prefix

--- backend/kernel/settings.py
+++ backend/kernel/settings.py
@@ -6,11 +6,11 @@ from decouple import Csv, config
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = config('BACKEND_SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = config('BACKEND_DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('BACKEND_ALLOWED_HOSTS', default='', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=Csv())
 
 DJANGO_APPS = [
     'django.contrib.admin',
