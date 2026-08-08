ac3023f797a71f24364b57527242a763e814e264
Luciano Martins <luciano.martins@quay.com.br>
2026-08-08 11:23:21 -0300

    chore(ui): remove branding footer from main layout

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -7,10 +7,6 @@
       <footer
         class="flex justify-end pt-2 pb-4 pr-8 text-[10px] text-slate-600 font-medium"
       >
-        <span
-          >Made by
-          <strong class="text-slate-400 font-semibold">Atoms</strong></span
-        >
       </footer>
     </q-page-container>
   </q-layout>
